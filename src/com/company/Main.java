b0cef41dd2e345a938c80e46149f5cdd52882d50
ihsankurt17 <102886451+ihsankurt17@users.noreply.github.com>
2022-04-11 19:54:00 +0200

    Test2 Message

---
+++ src/com/company/Main.java
@@ -0,0 +1,9 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Test2");
+        System.out.println("Test2 oldu");
+    }
+}
